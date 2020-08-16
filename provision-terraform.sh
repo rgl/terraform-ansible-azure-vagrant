52b3b277d8462043cda70a8226b4c4015f4de5ac
Rui Lopes <rgl@ruilopes.com>
2020-08-16 12:40:09 +0100

    install the ansible terraform provider at the machine level rather then in the user home

--- provision-terraform.sh
+++ provision-terraform.sh
@@ -15,19 +15,14 @@ install \
 rm terraform terraform_${terraform_version}_linux_amd64.zip
 
 # install the terraform ansible provider.
-# NB this has to be installed into the user home.
 wget -q https://github.com/nbering/terraform-provider-ansible/releases/download/v$terraform_provider_ansible_version/terraform-provider-ansible-linux_amd64.zip
 unzip terraform-provider-ansible-linux_amd64.zip
-terraform_provider_ansible_home="/home/vagrant/.terraform.d/plugins/registry.github.com.local/nbering/ansible/$terraform_provider_ansible_version/linux_amd64"
+terraform_provider_ansible_home="/usr/local/share/terraform/plugins/registry.github.com.local/nbering/ansible/$terraform_provider_ansible_version/linux_amd64"
 install \
   -d \
-  -o vagrant \
-  -g vagrant \
   $terraform_provider_ansible_home
 install \
   -m 755 \
-  -o vagrant \
-  -g vagrant \
   linux_amd64/terraform-provider-ansible_v$terraform_provider_ansible_version \
   $terraform_provider_ansible_home
 rm -rf linux_amd64 terraform-provider-ansible-linux_amd64.zip
